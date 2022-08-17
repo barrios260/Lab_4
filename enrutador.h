65be2df3a41ebe53d166b4eca53b65648d22be90
unknown <emanuel.barriosc@udea.edu.co>
2022-08-16 21:34:04 -0500

    inicios del lab

---
+++ enrutador.h
@@ -0,0 +1,30 @@
+#ifndef ENRUTADOR_H
+#define ENRUTADOR_H
+#include <iostream>
+#include <vector>
+#include <map>
+#include <fstream>
+
+using namespace std;
+
+class Enrutador
+{
+public:
+    Enrutador();
+    void lecturaCaminos(char router, string caminoRecorrido, char destino,char copi,char an,int ValorCami,int &B,string &caminoMayor);
+    void LectuArchi();
+    void Actualizacion();
+    void IngresoManualRouters();
+    vector<int> getValConexRuter() const;
+    void setValConexRuter(const vector<int> &value);
+    vector<string> getNomConexRuter()const;
+    void setNomConexRuter(const vector<string> &value);
+    map<char,vector<string>>getRouters()const;
+    void setRouters(const map<char,vector<string>>&value);
+private:
+    vector<int>valConexRuter;
+    vector<string>NomConexRuter;
+    map<char,vector<string>>routers;
+};
+
+#endif // ENRUTADOR_H
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+using namespace std;
+
+int main(){
+unsigned int opcion=1;
+while(opcion!=0){
+cout<<"Introduzca el numero que le aparece en pantalla para ingresar: "<<endl;
+cout<<"1.Ingresar nuevas conexiones o routers"<<endl;
+cout<<"2.Eliminar conexiones entre routers"<<endl;
+cout<<"3.Eliminar routers"<<endl;
+cout<<"4.Cambio el valor de la conexion entre routers"<<endl;
+cout<<"5.Mostrar el mejor camino entre un enrutador y otro"<<endl;
+cout<<"0.Salir"<<endl;
+cout<<"==> ";
+cin>>opcion;
+}
+switch(opcion){
+case 1:
+
+    break;
+}//switch
+
+}//main
